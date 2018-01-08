e1b4899d7a6cbaa863a2eaeb3121539b467b1f3f
mchaveza <knockmark10@hotmail.com>
2018-01-08 12:31:43 -0600

    Gif management not supported anymore

--- kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/ImageUtils.kt
+++ kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/ImageUtils.kt
@@ -3,13 +3,8 @@ package com.ia.mchaveza.kotlin_library
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
-import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
-import java.io.File
 
 /**
  * Created by mchaveza on 22/12/2017.
@@ -21,20 +16,6 @@ class ImageUtils(private val mContext: Context) {
         DrawableCompat.setTint(wrappedDrawable, ContextCompat.getColor(mContext.applicationContext, color))
         DrawableCompat.setTintMode(wrappedDrawable, PorterDuff.Mode.SRC_IN)
         return wrappedDrawable
-    }
-
-    fun loadGif(splashView: ImageView, @DrawableRes resource: Int){
-        val imageViewTarget = GlideDrawableImageViewTarget(splashView)
-        Glide.with(mContext)
-                .load(resource)
-                .into(imageViewTarget)
-    }
-
-    fun loadGifByUrl(splashView: ImageView, url: String){
-        val imageViewTarget = GlideDrawableImageViewTarget(splashView)
-        Glide.with(mContext)
-                .load(url)
-                .into(imageViewTarget)
     }
 
 }
