caccf39369268ad7cd85576e48f77bf67a8d7600
mchaveza <knockmark10@hotmail.com>
2018-02-20 12:43:37 -0600

    Geocoder improved

--- kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/GeocoderManager.kt
+++ kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/GeocoderManager.kt
@@ -12,11 +12,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 /**
  * Created by mchaveza on 19/12/2017.
  */
-class GeocoderManager(private val mContext: Context) {
+class GeocoderManager(private val mContext: Context, private val geoCoderCallback: GeoCoderCallback) {
 
     private var retrofit: Retrofit? = null
     private var interfaceAPI: InterfaceAPI? = null
-    private var geocoderMap: GeocoderResponse? = null
     private var error: String? = null
 
     fun getReverseGeocoder(latitude: Double, longitude: Double) {
@@ -30,12 +29,13 @@ class GeocoderManager(private val mContext: Context) {
         interfaceAPI?.getGeocoderData("json", latitude, longitude)?.enqueue(object : Callback<GeocoderResponse?> {
             override fun onResponse(call: Call<GeocoderResponse?>?, response: Response<GeocoderResponse?>?) {
                 response?.body()?.let { item ->
-                    geocoderMap = item
+                    geoCoderCallback.getData(item)
                 }
             }
 
             override fun onFailure(call: Call<GeocoderResponse?>?, t: Throwable?) {
                 t?.message?.let { errorMessage ->
+                    geoCoderCallback.getDataError(errorMessage)
                     error = errorMessage
                 }
             }
@@ -43,74 +43,68 @@ class GeocoderManager(private val mContext: Context) {
 
     }
 
-    fun getStreetName(): String {
+    fun getStreetName(geocoderMap: GeocoderResponse): String {
         var street = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                street = response.result[0]?.streetName.toString()
-            }
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            street = geocoderMap.result[0]?.streetName.toString()
         }
         return street
     }
 
-    fun getZipCode(): String {
+
+    fun getZipCode(geocoderMap: GeocoderResponse): String {
         var zipCode = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                zipCode = response.result[0]?.zipCode.toString()
-            }
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            zipCode = geocoderMap.result[0]?.zipCode.toString()
         }
         return zipCode
     }
 
-    fun getCity(): String {
+
+    fun getCity(geocoderMap: GeocoderResponse): String {
         var city = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                city = response.result[0]?.city.toString()
-            }
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            city = geocoderMap.result[0]?.city.toString()
         }
         return city
     }
 
-    fun getState(): String {
+    fun getState(geocoderMap: GeocoderResponse): String {
         var state = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                state = response.result[0]?.state.toString()
-            }
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            state = geocoderMap.result[0]?.state.toString()
         }
         return state
     }
 
-    fun getCountryCode(): String {
+    fun getCountryCode(geocoderMap: GeocoderResponse): String {
         var countryCode = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                countryCode = response.result[0]?.countryCode.toString()
-            }
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            countryCode = geocoderMap.result[0]?.countryCode.toString()
         }
         return countryCode
     }
 
-    fun getFormatedFull(): String {
-        var formatedFull = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                formatedFull = response.result[0]?.formatedFull.toString()
-            }
+    fun getFormatedFull(geocoderMap: GeocoderResponse): String {
+        var formattedFull = "No disponible"
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            formattedFull = geocoderMap.result[0]?.formatedFull.toString()
         }
-        return formatedFull
+        return formattedFull
     }
 
-    fun getFormatedPostal(): String {
-        var formatedPostal = "No disponible"
-        geocoderMap?.let { response ->
-            if (response.result != null && response.result.isNotEmpty()) {
-                formatedPostal = response.result[0]?.formatedPostal.toString()
-            }
+    fun getFormatedPostal(geocoderMap: GeocoderResponse): String {
+        var formattedPostal = "No disponible"
+        if (geocoderMap.result != null && geocoderMap.result.isNotEmpty()) {
+            formattedPostal = geocoderMap.result[0]?.formatedPostal.toString()
         }
-        return formatedPostal
+        return formattedPostal
+    }
+
+    interface GeoCoderCallback {
+        fun getData(response: GeocoderResponse)
+
+        fun getDataError(error: String)
     }
 
 }
