d42aadd651945aea84823492beeb8f06fea6ca27
mchaveza <mchaveza@ia.com.mx>
2018-05-25 13:42:25 -0500

    New extensions added

--- kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/ExtensionUtils.kt
+++ kotlin-library/src/main/java/com/ia/mchaveza/kotlin_library/ExtensionUtils.kt
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import java.util.*
 
 /**
  * Created by mchaveza on 22/12/2017.
@@ -24,3 +28,29 @@ fun FragmentManager.replaceFragment(containerViewId: Int, fragment: Fragment) {
             .replace(containerViewId, fragment)
             .commit()
 }
+
+fun FragmentManager.replaceFragmentAllowingStateLoss(containerViewId: Int, fragment: Fragment) {
+    this
+            .beginTransaction()
+            .replace(containerViewId, fragment)
+            .commitAllowingStateLoss()
+}
+
+fun ClosedRange<Int>.random() =
+        Random().nextInt(endInclusive - start) + start
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun ViewGroup.inflate(layoutRes: Int): View =
+        LayoutInflater.from(context).inflate(layoutRes, this, false)
+
