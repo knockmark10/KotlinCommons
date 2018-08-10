2dd57f43712bbc158097bc335ac287ea2ca493b2
mchaveza <mchaveza@ia.com.mx>
2018-08-10 12:17:46 -0500

    Main cleaned

--- app/src/main/java/com/ia/mchaveza/kotlin_commons/MainActivity.kt
+++ app/src/main/java/com/ia/mchaveza/kotlin_commons/MainActivity.kt
@@ -2,15 +2,11 @@ package com.ia.mchaveza.kotlin_commons
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.ia.mchaveza.kotlin_library.DateManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        DateManager.currentDateInMillis
-
     }
 }
